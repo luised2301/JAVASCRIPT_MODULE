ada3ae3da41cab362c30e515e4c64c0847deaceb
luised2301 <luised2301@gmail.com>
2022-07-14 21:18:13 -0500

    se agrega el ejercicio en clase de manejo de objetos

--- SESION_10_OBJETOS/script.js
+++ SESION_10_OBJETOS/script.js
@@ -1,25 +1,52 @@
-/**Ejercicio 1 
-* Realizar una función que reciba como parametro 1 array de numeros
-* Y devuelva un array con solo los elementos Pares de ese array
-* p.ej.
-* -> evenOnly( [1,2,3,4,18] ) -> [2,4,18]
-* -> evenOnly( [10, 2, 5] ) -> [10,2]
-* -> evenOnly() -> 'Se necesita un array'
-*
-* Pista / Hint, debemos iterar el array */
+// Dado el siguiente lleva a cabo las las operaciones indicadas
 
-const numbers = [1,4,56,7,88,4,3,6,7,3,3,5,7,8,4,6,8,5,7]
-const pairedNumbers = []
-console.log(`Mi arreglo tiene los siguientes valores: ${numbers}`);
-
-function arrayPair(a,b) {
-    for (let i = 0; i < a.length; i++) {
-        if ((a[i]%2)==0) {
-        b.push(a[i]);
-        }
-    }
+const player = {
+ name: "Eduardo",
+ score: 88888,
+ level: 7,
+ lives: 2,
+ vitalEnergy: 100,
+ isAlive: true,
+ damage: 125,
+};
+ 
+if (player.isAlive  == true  ){
+    console.log("El estatus del jugador es el siguiente");
+    console.log (player);
 }
+ 
+ 
+ 
+// ! 1 Jugador recibe un golpe crítico que le resta 25 puntos de energía vital "vital Energy"
+// Realizar código para llevar a cabo las operaciones
+console.log(`El jugador ${player.name} ha recibido un golpe critico de ${player.damage} puntos`)
 
-arrayPair(numbers, pairedNumbers);
-console.log(`Los valores pares del arreglo son: ${pairedNumbers}`);
+player.vitalEnergy= player.vitalEnergy-player.damage
 
+console.log(`Su nivel de energia ha bajado a ${player.vitalEnergy} `)
+
+ 
+// ! 2 Si la energía vital del jugador es menor a 0 pasado "isAlive" = false
+
+player.vitalEnergy  <=0 ? player.isAlive=false : player.isAlive=true
+
+
+
+// ! 3 Revisamos nuestro jugador
+player.isAlive  == false  ?  console.log("El jugador ha muerto!") : console.log ("Sigue vivo!")
+
+ 
+// ! 4  Si No está vivo pero tienen más vidas entonces -> Puntaje = 0, Vida -1, nivel = 0, energia vital = 100
+// Realizar código para llevar a cabo las operaciones
+
+if (player.isAlive  == false && player.lives >=1 ){
+    player.score= 0000;
+    player.lives = player.lives-1;
+    player.vitalEnergy =100;
+    player.isAlive= true;
+    console.log(`${player.name} ha resucitado, le quedan ${player.lives} vidas`)
+       };
+       if (player.isAlive  == true  ){
+        console.log("El estatus del jugador es el siguiente");
+        console.log (player);
+    }
