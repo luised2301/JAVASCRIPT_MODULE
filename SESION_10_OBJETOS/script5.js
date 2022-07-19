748fd9dcec974b62fc1be1363ed0505714501799
luised2301 <luised2301@gmail.com>
2022-07-18 19:59:56 -0500

    se agrega la carpeta de la sesion 11

---
+++ SESION_10_OBJETOS/script5.js
@@ -0,0 +1,19 @@
+explicacion metodo Assing
+
+// El método Object.assign() copia todas las propiedades enumerables de uno o más objetos fuente a un objeto destino. Devuelve el objeto destino. 
+
+const target = { a: 1, b: 2 };
+const source = { b: 4, c: 5 };
+
+const returnedTarget = Object.assign(target, source);
+
+console.log(target);
+// expected output: Object { a: 1, b: 4, c: 5 }
+
+console.log(returnedTarget);
+// expected output: Object { a: 1, b: 4, c: 5 }
+
+
+
+El método Object.entries() devuelve una matriz de pares propios de una propiedad enumerable [key, value] de un objeto dado, en el mismo orden 
+que es proporcionado por for...in (La diferencia es que un bucle for-in enumera las propiedades en la cadena de prototipos).
